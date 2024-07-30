96b6c9dd3ee28e610bdfb676fd83ebab29c06a0a
hakyg <haky.gamer050@gmail.com>
2024-07-30 17:35:16 +0300

    Добавьте файлы проекта.

---
+++ TcpClient/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace TcpClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region TCP
+            const string IP = "127.0.0.1";
+            const int PORT = 8080;
+
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(IP), PORT);
+
+            Console.WriteLine("Enter the message:");
+            var message = Console.ReadLine();
+
+            var data = Encoding.UTF8.GetBytes(message);
+
+            tcpSocket.Connect(tcpEndPoint);
+            tcpSocket.Send(data);
+
+            var buffer = new byte[256];
+            var trueSize = 0;
+            var answer = new StringBuilder();
+
+            do
+            {
+                trueSize = tcpSocket.Receive(buffer);
+                answer.Append(Encoding.UTF8.GetString(buffer, 0, trueSize));
+            }
+            while (tcpSocket.Available > 0);
+
+            Console.WriteLine(answer.ToString());
+            tcpSocket.Shutdown(SocketShutdown.Both);
+            tcpSocket.Close();
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
---
+++ TcpServer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpServer
+{
+    internal class Program
+    {
+        #region TCP
+        static void Main(string[] args)
+        {
+            const string IP = "127.0.0.1";
+            const int PORT = 8080;
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(IP), PORT);
+
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            tcpSocket.Bind(tcpEndPoint);
+            tcpSocket.Listen(5);
+
+            while (true)
+            {
+                var listener = tcpSocket.Accept();
+                var buffer = new byte[256];
+                var trueSize = 0;
+                var data = new StringBuilder();
+
+                do
+                {
+                    trueSize = listener.Receive(buffer);
+                    data.Append(Encoding.UTF8.GetString(buffer, 0, trueSize));
+                }
+                while (listener.Available > 0);
+
+                Console.WriteLine(data.ToString());
+
+                listener.Send(Encoding.UTF8.GetBytes("successfully!"));
+
+                listener.Shutdown(SocketShutdown.Both);
+                listener.Close();
+            }
+
+        }
+        #endregion
+    }
+}
